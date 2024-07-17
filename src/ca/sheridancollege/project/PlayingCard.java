49b8a86ea03d1cd2251a49dd4e4d849001b8d274
dilghuman <ghumadil@sheridancollege.ca>
2024-07-17 02:28:03 -0400

    merger to master

---
+++ src/ca/sheridancollege/project/PlayingCard.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author 991764920
+ */
+public class PlayingCard extends Card {
+    private int rank;
+    private String suit;
+
+    public PlayingCard(int rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
